f50deca0862e86bb3e3673ea9c5c2d8818a43e09
rumi <mujtaba.rumi1@gmail.com>
2017-07-31 18:04:39 +0600

    rumi

---
+++ sidebar.php
@@ -0,0 +1,217 @@
+
+<aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu">
+                  <li class="active">
+                      <a class="" href="home.php">
+                          <i class="icon_house_alt"></i>
+                          <span>Home</span>
+                      </a>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_documents_alt"></i>
+                          <span>Pages</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newPage.php">Create a new Page</a></li>
+                          <li><a class="" href="managePage.php"><span>Manage Page</span></a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_documents_alt"></i>
+                          <span>Pages Sections</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newPageSection.php">Create a new Sections</a></li>
+                          <li><a class="" href="managePage.php"><span>Manage Page Sections</span></a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_menu"></i>
+                          <span>Menu</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Menu</a></li>
+                          <li><a class="" href="managemenu.php">Manage Menu</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_book"></i>
+                          <span>Departments</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Departments</a></li>
+                          <li><a class="" href="managemenu.php">Manage Departments</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_book_alt"></i>
+                          <span>Courses</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Courses</a></li>
+                          <li><a class="" href="managemenu.php">Manage Courses</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_book_alt"></i>
+                          <span>Course Sections</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Sections</a></li>
+                          <li><a class="" href="managemenu.php">Manage Course Sections</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_book_alt"></i>
+                          <span>Course Units</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Units</a></li>
+                          <li><a class="" href="managemenu.php">Manage Course Units</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_book_alt"></i>
+                          <span> Unit Sections</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new  Sections</a></li>
+                          <li><a class="" href="managemenu.php">Manage Unit Sections</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_group"></i>
+                          <span>Users</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new User Roles</a></li>
+                          <li><a class="" href="managemenu.php">Manage User Roles</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_image"></i>
+                          <span>Photo</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Photo</a></li>
+                          <li><a class="" href="managemenu.php">Manage Photo</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_group"></i>
+                          <span>Faculties</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="create_new_faculty.php">Create a new Faculties</a></li>
+                          <li><a class="" href="managemenu.php">Manage Faculties</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_genius"></i>
+                          <span>Affiliations</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Affiliations</a></li>
+                          <li><a class="" href="managemenu.php">Manage Affiliations</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_clock"></i>
+                          <span>Events</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="create_new_events.php">Create a new Events</a></li>
+                          <li><a class="" href="managemenu.php">Manage Events</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_tag"></i>
+                          <span>News</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="create_new_news.php">Create a new News</a></li>
+                          <li><a class="" href="managemenu.php">Manage News</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_book_alt"></i>
+                          <span>RegisterInterest</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new </a></li>
+                          <li><a class="" href="managemenu.php">Manage RegisterInterests</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_comment"></i>
+                          <span>Feedbacks</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new Feedbacks</a></li>
+                          <li><a class="" href="managemenu.php">Manage Feedbacks</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_contacts"></i>
+                          <span>ContactUs</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new ContactUs</a></li>
+                          <li><a class="" href="managemenu.php">Manage ContactUs</a></li>
+                      </ul>
+                  </li>
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_info"></i>
+                          <span>CollegeInfo</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="newmenu.php">Create a new CollegeInfo</a></li>
+                          <li><a class="" href="managemenu.php">Manage CollegeInfo</a></li>
+                      </ul>
+                  </li>
+
+
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
